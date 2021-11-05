30522791f9246fe2c2e7eb8ac33727f5b058fd7c
tim <tja2353@gmail.com>
2021-11-05 11:31:14 +0000

    (package.json) firebase-tools 9.22.0

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
