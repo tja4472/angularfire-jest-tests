86565d93e113cb33b897c4ccf77f7c3a7bf29ed7
Tim <tja2353@gmail.com>
2022-01-17 16:56:07 +0000

    (package.json) firebase 9.6.3 @types/jest 27.4.0 firebase-tools 10.1.0 jest 27.4.7

--- src/app/issue-compat.spec.ts
+++ src/app/issue-compat.spec.ts
@@ -1,23 +1,22 @@
 /**
- * @jest-environment jsdom
+ * @jest-environment node
  */
-
-/*
-  Upgrade to 9.2.0 gives error in jest unit tests
-  https://github.com/firebase/firebase-js-sdk/issues/5687
-*/
-
 import { TestBed } from '@angular/core/testing';
 import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting,
-} from '@angular/platform-browser-dynamic/testing';
-
-import { AngularFireModule, FirebaseApp } from '@angular/fire/compat';
+  AngularFireModule,
+  FirebaseApp,
+  FIREBASE_APP_NAME,
+  FIREBASE_OPTIONS,
+} from '@angular/fire/compat';
 import {
   AngularFirestoreModule,
   AngularFirestore,
+  USE_EMULATOR,
+  AngularFirestoreDocument,
+  AngularFirestoreCollection,
+  DocumentData,
 } from '@angular/fire/compat/firestore';
+import 'firebase/compat/firestore';
 
 const randomString = () => (Math.random() + 1).toString(36).split('.')[1];
 
@@ -25,52 +24,121 @@ const rando = () => [randomString(), randomString(), randomString()].join('');
 
 const COMMON_CONFIG = {
   apiKey: 'AIzaSyBVSy3YpkVGiKXbbxeK0qBnu3-MNZ9UIjA',
-  authDomain: 'angularfire2-test.firebaseapp.com',
-  databaseURL: 'https://angularfire2-test.firebaseio.com',
+  // authDomain: 'angularfire2-test.firebaseapp.com',
+  // databaseURL: 'https://angularfire2-test.firebaseio.com',
   projectId: 'demo-1',
-  storageBucket: 'angularfire2-test.appspot.com',
-  messagingSenderId: '920323787688',
-  appId: '1:920323787688:web:2253a0e5eb5b9a8b',
-  databaseName: 'angularfire2-test',
-  measurementId: 'G-W20QDV5CZP',
+  // storageBucket: 'angularfire2-test.appspot.com',
+  // messagingSenderId: '920323787688',
+  // appId: '1:920323787688:web:2253a0e5eb5b9a8b',
+  // databaseName: 'angularfire2-test',
+  // measurementId: 'G-W20QDV5CZP',
 };
 
-describe.skip('Firestore', () => {
+// Based on https://github.com/angular/angularfire/blob/master/src/compat/firestore/firestore.spec.ts
+describe('(compat)AngularFirestore', () => {
   let app: FirebaseApp;
   let firestore: AngularFirestore;
-  let appName: string;
 
-  describe('single injection', () => {
-    /*    
-    beforeAll(() => {
-      TestBed.resetTestEnvironment();
-      TestBed.initTestEnvironment(
-        BrowserDynamicTestingModule,
-        platformBrowserDynamicTesting()
-      );
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(COMMON_CONFIG),
+        AngularFirestoreModule,
+      ],
+      providers: [{ provide: USE_EMULATOR, useValue: ['localhost', 8080] }],
+      teardown: { destroyAfterEach: false },
     });
-*/
-    beforeEach(() => {
-      appName = rando();
-      TestBed.configureTestingModule({
-        imports: [
-          AngularFireModule.initializeApp(COMMON_CONFIG),
-          AngularFirestoreModule,
-        ],
-        teardown: { destroyAfterEach: false },
-      });
-      app = TestBed.inject(FirebaseApp);
-      firestore = TestBed.inject(AngularFirestore);
+    app = TestBed.inject(FirebaseApp);
+    firestore = TestBed.inject(AngularFirestore);
+  });
+
+  afterEach(() => {
+    app.delete().catch(() => undefined);
+  });
+
+  it('should be the properly initialized type', () => {
+    expect(firestore instanceof AngularFirestore).toBe(true);
+  });
+
+  it('should have an initialized Firebase app', () => {
+    expect(firestore.firestore.app).toBeDefined();
+  });
+
+  it('should create an AngularFirestoreDocument from a string path', () => {
+    const doc = firestore.doc('a/doc');
+    expect(doc instanceof AngularFirestoreDocument).toBe(true);
+  });
+
+  it('should create an AngularFirestoreDocument from a reference', () => {
+    const reference = firestore.doc<DocumentData>('a/doc').ref;
+    const doc = firestore.doc(reference);
+    expect(doc instanceof AngularFirestoreDocument).toBe(true);
+  });
+
+  it('should create an AngularFirestoreCollection from a string path', () => {
+    const collection = firestore.collection('stuffs');
+    expect(collection instanceof AngularFirestoreCollection).toBe(true);
+  });
+
+  it('should create an AngularFirestoreCollection from a reference', () => {
+    const reference = firestore.collection<DocumentData>('stuffs').ref;
+    const collection = firestore.collection(reference);
+    expect(collection instanceof AngularFirestoreCollection).toBe(true);
+  });
+
+  it('should throw on an invalid document path', () => {
+    const singleWrapper = () => firestore.doc('collection');
+    const tripleWrapper = () => firestore.doc('collection/doc/subcollection');
+    expect(singleWrapper).toThrowError();
+    expect(tripleWrapper).toThrowError();
+  });
+
+  it('should throw on an invalid collection path', () => {
+    const singleWrapper = () => firestore.collection('collection/doc');
+    const quadWrapper = () =>
+      firestore.collection('collection/doc/subcollection/doc');
+    expect(singleWrapper).toThrowError();
+    expect(quadWrapper).toThrowError();
+  });
+});
+
+describe('(compat)AngularFirestore with different app', () => {
+  let app: FirebaseApp;
+  let afs: AngularFirestore;
+  let firebaseAppName: string;
+
+  beforeEach(() => {
+    firebaseAppName = rando();
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(COMMON_CONFIG, firebaseAppName),
+        AngularFirestoreModule,
+      ],
+      providers: [
+        { provide: FIREBASE_APP_NAME, useValue: firebaseAppName },
+        { provide: FIREBASE_OPTIONS, useValue: COMMON_CONFIG },
+        { provide: USE_EMULATOR, useValue: ['localhost', 8080] },
+      ],
+      teardown: { destroyAfterEach: false },
     });
 
-    afterEach(() => {
-      // deleteApp(app).catch(() => undefined);
-    });
+    app = TestBed.inject(FirebaseApp);
+    afs = TestBed.inject(AngularFirestore);
+  });
 
-    it('should be injectable', () => {
-      expect(firestore).toBeTruthy();
-      // expect(firestore).toEqual(providedFirestore);
-      // expect(firestore.app).toEqual(app);
-    });
+  afterEach(() => {
+    app.delete().catch(() => undefined);
+  });
+
+  it('should be an AngularFirestore type', () => {
+    expect(afs instanceof AngularFirestore).toEqual(true);
+  });
+
+  it('should have an initialized Firebase app', () => {
+    expect(afs.firestore.app).toBeDefined();
+  });
+
+  it('should have an initialized Firebase app instance member', () => {
+    expect(afs.firestore.app.name).toEqual(firebaseAppName);
   });
 });
